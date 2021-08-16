b0b4bf55deae5e8ff599d81095e5f9991046c325
Khanh Nguyen <nhkhanh2406@gmail.com>
2021-08-16 21:31:19 +0700

    #10 validate gcaptcharesponse with google recaptcha

--- http/src/main/java/com/huongdanjava/mvnforum/http/GoogleReCaptchaAdapterImpl.java
+++ http/src/main/java/com/huongdanjava/mvnforum/http/GoogleReCaptchaAdapterImpl.java
@@ -3,6 +3,7 @@ package com.huongdanjava.mvnforum.http;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -10,10 +11,14 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.NameValuePair;
+import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.huongdanjava.mvnforum.usecases.adapter.GoogleReCaptchaAdapter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class GoogleReCaptchaAdapterImpl implements GoogleReCaptchaAdapter {
 
   public static final String SITE_VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify";
@@ -24,15 +29,28 @@ public class GoogleReCaptchaAdapterImpl implements GoogleReCaptchaAdapter {
       HttpPost httpPost = new HttpPost(SITE_VERIFY_URL);
 
       List<NameValuePair> nvps = new ArrayList<>();
-      nvps.add(new BasicNameValuePair("secret", secretKey));
       nvps.add(new BasicNameValuePair("response", gRecaptchaResponse));
-      httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+      nvps.add(new BasicNameValuePair("secret", secretKey));
+      try (UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(nvps)) {
+        httpPost.setEntity(formEntity);
 
-      try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
-        System.out.println(response.getCode() + " " + response.getReasonPhrase());
-        HttpEntity entity = response.getEntity();
+        try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
+          System.out.println(response.getCode() + " " + response.getReasonPhrase());
+          if (response.getCode() != 200) {
+            return false;
+          }
 
-        EntityUtils.consume(entity);
+          try (HttpEntity entity = response.getEntity()) {
+            String result = EntityUtils.toString(entity);
+
+            ObjectMapper om = new ObjectMapper();
+            Map<String, Object> map = om.readValue(result, Map.class);
+
+            return ((Boolean) map.get("success")).booleanValue();
+          }
+        } catch (ParseException e) {
+          log.error(e.getMessage(), e);
+        }
       }
     }
 
